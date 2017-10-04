525789fd4f96c6bd688650e2e9ec23576b9486cd
Sberry5 <shannon.berryheller@gmail.com>
2017-10-04 13:01:19 -0500

    progess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,70 @@
-//Array from which number will be randomly chosen
-var numberBank=["24", "37", "28", "35", "39", "14", "25", "33", "30", "17", "15"];
-
-var crystalValue=[
-	"12", "8", "2", "9", "16", "20", "10", "4",
-	"14", "5", "7", "17", "18", "3", "26", "1"];
-
 //--------------------This Works---------------------
+$(document).ready(function(){
+
 //Variable to store the games random number
-var computerScore = createnumber(numberBank);
+var computerScore = computerNumber();
 console.log(computerScore);
 
-//Variable to store red crystals random number
-var redScore = createnumber (crystalValue);
+var redScore = createnumber();
 console.log(redScore);
 
-var blueScore = createnumber (crystalValue);
+var blueScore = createnumber();
 console.log(blueScore);
 
-var yellowScore = createnumber (crystalValue);
+var yellowScore = createnumber();
 console.log(yellowScore);
 
-var greenScore = createnumber (crystalValue);
+var greenScore = createnumber();
 console.log(greenScore);
 
-//Function to select random number from array
-	function createnumber (myArray) {
-		return myArray[Math.floor(Math.random() * myArray.length)];
+var userScore = 0;
+
+var wins = 0;
+
+var losses = 0;
+
+
+
+//Function to create a random number for each crystal
+	function createnumber () {
+		return Math.floor(Math.random() * 12) + 1;
+
 	}
 
-//Function to place games random number in div
-document.getElementById("gameNumber").innerHTML = computerScore;
+//Function to create a random number for the computer score
+	function computerNumber () {
+		return Math.floor(Math.random() * 120) + 19;
 
-//Function to turn red crystal into a button
-//$(document).ready(function(){
-	$("#red,#green,#blue,#yellow").on("click", function() {
+	}
+
+//Function to initialize game
+
+
+//Function to place games random number in div
+$("#gameNumber").text(computerScore);
+
+//When crystal is click add number to userScores div
+$("#red,#green,#blue,#yellow").on("click", function() {
+	var userScore = (redScore + blueScore + yellowScore + greenScore);
+	userScore += 
+});
+
+//if (userScore === computerScore){
+//	wins++;
+//}
+
+//if (userScore >= computerScore){
+//	losses++;
+//}
+
+
+
 
 
 //Console.log to check if clicking on image works
-//console.log("yay");
-	});
-//});
-
-//Function to assign random number from array to each image
+//console.log("yay")
 
 //Populate number of losses
+
+
 //Populate number of wins
